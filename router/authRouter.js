a4469857aeecfdfc6240bdf183ee997058b19d4a
Alejo Calandra <alejocalandra18@gmail.com>
2024-07-01 20:47:08 -0300

    saludos

--- router/authRouter.js
+++ router/authRouter.js
@@ -3,7 +3,7 @@ const { crearUsuario, loginUsuario } = require('../controllers/authControllers')
 const routerAuth = express.Router();
 
 routerAuth.post('/login', loginUsuario);
-
+console.log('hola');
 routerAuth.post('/registro', crearUsuario);
 
 module.exports = routerAuth;
